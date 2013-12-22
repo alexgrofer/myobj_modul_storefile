f1c7752090a7f4581e024c63d265082eb2a64182
alexgro <you@example.com>
2013-12-22 18:18:18 +0400

    test

--- myobj/controllers/cms/sys/storefile/defmodel.php
+++ myobj/controllers/cms/sys/storefile/defmodel.php
@@ -16,39 +16,42 @@ if($modelAD->is_del && $modelAD->indexEdit!='') {
 //если добавил файл или файлы,все остальные действия перекладываем на плагин и модель
 if((isset($isValidate) && $isValidate) && $this->dicturls['action']=='edit') {
 	$files = CUploadedFile::getInstancesByName(get_class($modelAD).'[fileAdd]');
-	if($files) {
-		//заливка множества файлов
-		if(count($files)>1) {
-			/* ФАЙЛЫ */
-			$modelAD::$thiObjFile->filesMany = $files;
-			/* РАНДОМНЫЕ ИМЕНА */
-			if($modelAD->is_randName) {
-				$modelAD::$thiObjFile->isRandMany = true;
-			}
-			/* ... */
+
+	//заливка множества файлов
+	if(count($files)>1) {
+		/* ФАЙЛЫ */
+		$modelAD::$thiObjFile->filesMany = $files;
+		/* РАНДОМНЫЕ ИМЕНА */
+		if($modelAD->is_randName) {
+			$modelAD::$thiObjFile->isRandMany = true;
 		}
-		//в случае с отдельным файлом или редактировании отдельного элемента
-		else {
-			if($modelAD->indexEdit!='') {
-				$indexEdit = $modelAD->indexEdit;
-			}
-			else {
-				$indexEdit = $modelAD::$thiObjFile->objPlugin->getNextIndex();
-			}
-			/* ФАЙЛ */
-			if($files) {
-				$modelAD::$thiObjFile->setFile($files[0],$indexEdit);
-			}
-			/* ИМЯ ФАЙЛА */
-			if($modelAD->is_randName) {
-				static::$thiObjFile->is($indexEdit);
-			}
-			elseif($modelAD->nameFile!='') {
-				static::$thiObjFile->setName($indexEdit);
-			}
-			/* ... */
-		}
+		/* ... */
 	}
+	//в случае с отдельным файлом или редактировании отдельного элемента
+	else {
+		if($modelAD->indexEdit!='') {
+			$indexEdit = $modelAD->indexEdit;
+		}
+		else {
+			$indexEdit = $modelAD::$thiObjFile->objPlugin->getNextIndex();
+		}
+		/* ФАЙЛ */
+		if($files) {
+			$modelAD::$thiObjFile->setFile($files[0],$indexEdit);
+		}
+		/* ИМЯ ФАЙЛА */
+		if($modelAD->is_randName) {
+			static::$thiObjFile->is($indexEdit);
+		}
+		elseif($modelAD->nameFile!='') {
+			static::$thiObjFile->setName($indexEdit);
+		}
+		/* ... */
+	}
+
+	static::$thiObjFile->save();
+
+	$this->redirect(Yii::app()->request->url);
 }
 
 
